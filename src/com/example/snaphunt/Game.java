a5eaa9107d4ac950424675e72b4ead2ffb842c27
James Bilous <james.bilous@gmail.com>
2013-12-02 21:10:41 -0800

    Registration added, Games and Invites now going to arrays

---
+++ src/com/example/snaphunt/Game.java
@@ -0,0 +1,32 @@
+package com.example.snaphunt;
+
+public class Game {
+
+	String id, p1username, p2username, p3username, p4username, groupName, theme;
+	Game() {
+
+	}
+	public String getp1name() {
+		return p1username;
+	}
+
+	public String getp2name() {
+		return p2username;
+	}
+
+	public String getp3name() {
+		return p3username;
+	}
+
+	public String getp4name() {
+		return p4username;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public String getId() {
+		return id;
+	}
+}
